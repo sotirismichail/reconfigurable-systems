bfacf4e34e00271fed196728977e6dfdba6e644f
Sotiris Michail <michailsotiris@outlook.com>
2022-11-07 17:41:37 +0200

    initial commit

---
+++ accelerator_dim16/src/main.c
@@ -0,0 +1,171 @@
+#include "myLib.h"
+
+void myFunc (unsigned int size, unsigned int dim, dataType_t threshold, dataType_t * data0, dataType_t * data1, dataType_t * data2)
+{
+	unsigned int i, k, l;
+
+	for ( i = 0 ; i < size ; i ++ )
+	{
+		for ( k = 0 ; k < dim ; k ++ )
+		{
+			data2 [ i*dim + k ] = 0.0 ;
+		}
+
+		for ( k = 0 ; k < dim ; k ++ )
+		{
+			for ( l = 0 ; l < dim ; l ++ )
+			{
+				data2 [ i*dim + k ] += data0 [ k * dim + l ] * data1 [ i*dim+ l ];
+			}			
+		}
+		
+		int r = 1 ;
+
+		for ( l = 0 ; r && ( l < dim ) ; l ++ )
+		{
+			r = ( data2 [ i*dim + l ] > threshold ) ;
+		}
+
+		if ( r )
+		{
+			for ( l = 0 ; l < dim ; l ++ )
+			{
+				data2 [ i*dim + l ] = 0.0;
+			}
+		}
+	}
+}
+
+int main(int argc, char ** argv)
+{
+
+    /*
+     * Timing variables
+     */
+    double totalTime_sw=0.0;
+	struct timespec timerStart_sw;
+	struct timespec timerStop_sw;
+    
+    double totalTime_hw=0.0;
+	struct timespec timerStart_hw;
+	struct timespec timerStop_hw;
+    
+	unsigned int i;
+
+	assert(argv[1]!=NULL);
+	unsigned int seed = (unsigned int)atoi(argv[1]);
+	assert(seed);
+        
+    /*
+     * rand() seed
+     */
+	srand(seed);
+
+	assert(argv[2] != NULL);
+	unsigned int size = (unsigned int)atoi(argv[2]);
+	assert(size>=1);
+
+	assert(argv[3] != NULL);
+	unsigned int dim = (unsigned int)atoi(argv[3]);
+	assert(dim>=2);
+
+	assert(argv[4] != NULL);
+	dataType_t threshold = (dataType_t)atof(argv[4]);
+	assert(threshold>=0.0);
+
+	printf("Seed %u\nSize %u\nDimension %u\nThreshold %.2f\n", seed, size, dim, threshold);
+	fflush(stdout);	
+
+	dataType_t * data0 = (dataType_t *)sds_alloc(sizeof(dataType_t)*dim*dim);
+	assert(data0!=NULL);
+
+	for(i=0;i<dim*dim;i++) {
+		dataType_t t = (float)(rand()%10);
+		dataType_t d = ((float)(rand()%10))/10;
+		data0[i] = t+d;
+        assert(data0[i] == t+d);
+	}
+	
+	dataType_t * data1 = (dataType_t *)sds_alloc(sizeof(dataType_t)*dim*size);
+	assert(data1!=NULL);
+
+	for(i=0;i<dim*size;i++) {
+		dataType_t t = (float)(rand()%10);
+		dataType_t d = ((float)(rand()%10))/10;
+		data1[i] = t+d;
+        assert(data1[i] == t+d);
+	}
+	
+	/********************************/
+    /* Reference Software Execution */
+    /********************************/
+
+	dataType_t * data2_sw = (dataType_t *)malloc(sizeof(dataType_t)*dim*size);
+	assert(data2_sw!=NULL);
+    
+	printf("Calling myFunc... ");
+	fflush(stdout);
+    
+    /*
+     * Start timing
+     */
+    clock_gettime(CLOCK_REALTIME, &timerStart_sw);
+    
+	myFunc (size, dim, threshold, data0, data1, data2_sw);
+    /*
+     * Stop timing and get function running time
+     */
+    clock_gettime(CLOCK_REALTIME, &timerStop_sw);
+    totalTime_sw = (timerStop_sw.tv_sec-timerStart_sw.tv_sec)+(timerStop_sw.tv_nsec-timerStart_sw.tv_nsec)/BILLION;
+	printf("DONE\n");
+	fflush(stdout);
+    
+    printf("Software execution time: %f seconds\n", totalTime_sw);
+    
+    /**********************/
+    /* Hardware Execution */
+    /**********************/
+
+    /*
+     * Determine the indeces of the split array for multi instance accelerator
+     */
+    int size0 = size/2;
+    int size1 = size - size0;
+
+	dataType_t * data2_hw = (dataType_t *)sds_alloc(sizeof(dataType_t)*dim*size);
+	assert(data2_hw!=NULL);
+    
+    printf("Calling myFuncAccel... ");
+    fflush(stdout);
+    /*
+     * Start timing
+     */
+    clock_gettime(CLOCK_REALTIME, &timerStart_hw);
+	myFuncAccel (size, dim, threshold, data0, data1, data2_hw);
+    /*
+     * Stop timing and get function running time
+     */
+    clock_gettime(CLOCK_REALTIME, &timerStop_hw);
+    totalTime_hw = (timerStop_hw.tv_sec-timerStart_hw.tv_sec)+(timerStop_hw.tv_nsec-timerStart_hw.tv_nsec)/BILLION;
+	printf("DONE\n");
+	fflush(stdout);
+    
+    printf("Hardware execution time: %f seconds\n", totalTime_hw);
+    
+    /*****************************************************/
+    /* Correctness check/Compare with software reference */
+    /*****************************************************/
+    
+    for (i=0; i<size; i++) {
+        for (int k=0; k<dim; k++) {
+        	assert(data2_sw[i*dim + k] == data2_hw[i*dim + k]);
+        }
+    }
+    
+	sds_free(data0);
+	sds_free(data1);
+	free(data2_sw);
+    sds_free(data2_hw);
+
+	return 0;
+}
---
+++ accelerator_dim4/src/myIP.c
@@ -0,0 +1,98 @@
+#include "myLib.h"
+
+#define dimf 4
+#define bufdim 4
+#define acache 16
+
+#pragma SDS data access_pattern(data0:SEQUENTIAL)
+#pragma SDS data access_pattern(data1:SEQUENTIAL)
+#pragma SDS data access_pattern(data2:SEQUENTIAL)
+
+#pragma SDS data copy(data0[0:dim*dimf])
+#pragma SDS data copy(data1[0:size*dimf])
+#pragma SDS data copy(data2[0:size*dimf])
+
+#pragma SDS data data_mover(data0:AXIDMA_SIMPLE:1, data1:AXIDMA_SIMPLE:2, data2:AXIDMA_SIMPLE:3)
+
+#pragma SDS data buffer_depth(data0buf:16, data1buf:4, data2buf:4, r:4)
+
+void myFuncAccel (unsigned int size, unsigned int dim, dataType_t threshold, dataType_t * data0, dataType_t * data1, dataType_t * data2)
+{
+	unsigned int i, k, l;
+    dataType_t data0buf[acache], data1buf[bufdim], data2buf[bufdim];
+    int r[4];
+
+#pragma HLS ARRAY_PARTITION variable=data2buf block factor=4 dim=1
+#pragma HLS ARRAY_PARTITION variable=data1buf block factor=4 dim=1
+#pragma HLS ARRAY_PARTITION variable=data0buf block factor=4 dim=1
+
+    data0_loop:for ( k = 0 ; k < dimf ; k ++ )
+    {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS PIPELINE II=4
+        data0_loop_inner:for ( l = 0 ; l < dimf; l ++ )
+        {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS UNROLL
+            data0buf [ k * dimf + l ] = data0 [ k * dimf + l ];
+        }
+    }
+
+	i_loop:for ( i = 0 ; i < size ; i ++ )
+	{
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS PIPELINE II=4
+		init_loop:for ( k = 0 ; k < dimf ; k ++ )
+		{
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS UNROLL
+			data2 [ i * dimf + k ] = 0.0;
+            data2buf [ k ] = 0.0;
+		}
+
+        l_loop:for ( l = 0 ; l < dimf ; l ++ )
+        {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS UNROLL
+                data1buf [ l ] = data1 [ i * dimf + l ];
+        }
+
+		k_loop_outer:for ( k = 0 ; k < dimf ; k ++ )
+		{
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+			k_loop_inner:for ( l = 0 ; l < dimf ; l ++ )
+			{
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS UNROLL
+                data2buf [ k ] += data0buf [ k * dimf + l ] * data1buf [ l ];
+			}			
+		}
+
+		for ( l = 0 ; l < dimf ; l ++ )
+		{
+#pragma HLS UNROLL
+			r[ l ] = 1;
+			r[ l ] = ( data2buf [ l ] > threshold );
+		}
+
+//int rf = ( r[0] == 1 && r[1] == 1 && r[2] == 1 && r[3] == 1 && r[4] == 1 && r[5] == 1 && r[6] == 1 && r[7] == 1 && r[8] == 1 && r[9] == 1 && r[10] == 1 && r[11] == 1 && r[12] == 1 && r[13] == 1 && r[14] == 1 &&  r[15] == 1);
+int rf = ( r[0] == 1 && r[1] == 1 && r[2] == 1 && r[3] == 1 );
+
+		if ( rf )
+		{
+			thres_apply:for ( l = 0 ; l < dimf ; l ++ )
+			{
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS UNROLL
+				data2buf [ l ] = 0.0;
+			}
+		}
+
+		output:for( k = 0 ; k < dimf ; k ++ )
+        {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=4 avg=4
+#pragma HLS UNROLL
+            data2 [ i * dimf + k ] = data2buf [ k ];
+        }
+    }
+}
